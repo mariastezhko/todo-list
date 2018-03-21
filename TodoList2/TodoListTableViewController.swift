41b1c8d4e95939923084cd1e6037ba9358b562f4
Maria Stezhko <stezhko@gmail.com>
2018-03-21 14:51:48 -0700

    feat: Add functionality to delete item

--- TodoList2/TodoListTableViewController.swift
+++ TodoList2/TodoListTableViewController.swift
@@ -139,17 +139,27 @@ class TodoListTableViewController: UITableViewController, AddItemViewControllerD
     }
     */
 
-    /*
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+//        if editingStyle == .delete {
+//            // Delete the row from the data source
+//            tableView.deleteRows(at: [indexPath], with: .fade)
+//        } else if editingStyle == .insert {
+//            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+//        }
+        let item = items[indexPath.row]
+        managedObjectContext.delete(item)
+        
+        do {
+            try managedObjectContext.save()
+        } catch {
+            print(error)
+        }
+        items.remove(at: indexPath.row)
+        tableView.reloadData()
     }
-    */
+    
 
     /*
     // Override to support rearranging the table view.
